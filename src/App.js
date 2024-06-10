279f83b664d1383e46bc6257dcf44bf0f7fc2397
dayeon <dayon0820@naver.com>
2024-06-11 00:20:10 +0900

    coin-converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState({});
+  const [budget, setBudget] = useState(0);
+  const [result, setResult] = useState(0);
+  const onChange = (event) => {
+    setBudget(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setResult(Math.floor(budget / coin.quotes.USD.price));
+  };
+  const onselect = (event) => {
+    const coin_id = event.target.value;
+    const coin = coins.find((ele) => ele.id == coin_id);
+    setCoin(coin);
+  };
+  useEffect(() => {
+    console.log("Coin selected:", coin.name);
+    console.log("Budget:", budget);
+  }, [coin, budget]);
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <select onChange={onselect}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={onChange}
+              value={budget}
+              placeholder="your budget (USD)"
+              type="number"
+            />
+            <button>submit</button>
+          </form>
+          <h2>
+            you can buy {result} {coin.symbol}
+          </h2>
+        </>
+      )}
     </div>
   );
 }
